539edbdd91eb1e6ad94b3a4d9521ce3d582f5b09
MaxZay <54762301+MaxZay@users.noreply.github.com>
2021-02-17 14:33:51 +0300

    carbon-dating решено

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -1,9 +1,22 @@
 const CustomError = require("../extensions/custom-error");
 
-const MODERN_ACTIVITY= 15; 
-const HALF_LIFE_PERIOD= 5730;
+const MODERN_ACTIVITY = 15;
+const HALF_LIFE_PERIOD = 5730;
 
-module.exports = function dateSample(/* sampleActivity */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function dateSample(sampleActivity) {
+  if (sampleActivity === undefined || typeof sampleActivity != "string")
+    return false
+  if (Number(sampleActivity) != NaN && Number(sampleActivity) <= 15 && Number(sampleActivity) > 0)
+    return Math.ceil(MODERN_ACTIVITY / Number(sampleActivity) * HALF_LIFE_PERIOD)
+  return false
 };
+
+/*function test(sampleActivity) {
+  if (sampleActivity === undefined || typeof sampleActivity != "string")
+    return false
+  if (Number(sampleActivity) != NaN && Number(sampleActivity) <= 15 && Number(sampleActivity) > 0)
+    return Math.ceil(MODERN_ACTIVITY / Number(sampleActivity) * HALF_LIFE_PERIOD)
+  return false
+}
+
+console.log(test('ACTIVITY OVER 9000'));*/
